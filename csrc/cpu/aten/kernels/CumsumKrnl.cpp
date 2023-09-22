fab7a41e6990527e71053868f70067dcd3015629
ZheWang <zhe1.wang@intel.com>
2023-09-22 03:08:59 -0700

    replace op demo

--- csrc/cpu/aten/kernels/CumsumKrnl.cpp
+++ csrc/cpu/aten/kernels/CumsumKrnl.cpp
@@ -1,5 +1,6 @@
 #include <ATen/NamedTensorUtils.h>
 #include <ATen/WrapDimUtils.h>
+#include <ATen/core/TensorBody.h>
 #include <ATen/cpu/vec/functional.h>
 #include <ATen/cpu/vec/vec.h>
 #include <ATen/native/ReduceOpsUtils.h>
@@ -22,106 +23,21 @@ inline int64_t divup(int64_t x, int64_t y) {
   return (x + y - 1) / y;
 }
 
-template <typename scalar_t>
 static inline void cumsum_lastdim_kernel(
-    at::Tensor& result,
-    const at::Tensor& self,
-    int64_t dim) {
-  const auto input_ndim = self.dim();
-  std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-  TORCH_CHECK(
-      dim == input_ndim - 1,
-      "cumsum_lastdim_kernel: expect dim to be ",
-      input_ndim - 1,
-      " got ",
-      dim);
-  TORCH_CHECK(
-      self.scalar_type() == result.scalar_type(),
-      "cumsum_lastdim_kernel: expect same data type for self and result");
-
-  if (result.sizes() != self.sizes()) {
-    result.resize_as_(self);
-  }
-  if (self.numel() == 0) {
-    return;
-  }
-  if (input_ndim == 0) {
-    result.fill_(self);
-    return;
-  }
-
-  int64_t N = self.size(dim);
-  int64_t M = self.numel() / N;
-  const scalar_t* self_data = self.data_ptr<scalar_t>();
-  scalar_t* result_data = result.data_ptr<scalar_t>();
-
-  int64_t T = at::get_num_threads();
-
-  // bytes per core for each chunk, set to 256KB (L2 cache reside)
-  constexpr int64_t CHUNK_SIZE_PER_CORE = 256 * 1024 / sizeof(scalar_t);
-  int64_t CHUNK_SIZE = std::max(int64_t(1), CHUNK_SIZE_PER_CORE / M * T);
-  int64_t K = divup(N, CHUNK_SIZE);
-
-  // offset value per chunk
-  std::vector<scalar_t> outer_offsets(M, scalar_t(0));
-
-  // offset value per thread
-  std::vector<scalar_t> inner_offsets(M * T, scalar_t(0));
-
-  for (int64_t k = 0; k < K; k++) {
-    int64_t k_begin = k * CHUNK_SIZE;
-    int64_t k_end = std::min(k_begin + CHUNK_SIZE, N);
-
-    // Parallel Path I: accumulate locally per thread
-    at::parallel_for(k_begin, k_end, 1, [&](int64_t begin, int64_t end) {
-      int64_t tid = at::get_thread_num();
-      for (int64_t m = 0; m < M; m++) {
-        const scalar_t* self_ptr = self_data + m * N + begin;
-        scalar_t* result_ptr = result_data + m * N + begin;
-        int64_t len = end - begin;
-
-        prefix_sum<scalar_t>(self_ptr, result_ptr, scalar_t(0), len);
-        inner_offsets[m * T + tid] = result_ptr[len - 1];
-      }
-    });
-
-    // update offset value for each thread
-    for (int64_t m = 0; m < M; m++) {
-      for (int64_t t = T - 1; t >= 0; t--) {
-        scalar_t offset = scalar_t(0);
-        for (int64_t i = t - 1; i >= 0; i--) {
-          offset += inner_offsets[m * T + i];
-        }
-        inner_offsets[m * T + t] = offset;
-      }
-    }
-
-    // Parallel Path II: apply offset (result should be in L2)
-    at::parallel_for(k_begin, k_end, 1, [&](int64_t begin, int64_t end) {
-      int64_t tid = at::get_thread_num();
-      for (int64_t m = 0; m < M; m++) {
-        scalar_t* result_ptr = result_data + m * N + begin;
-        int64_t len = end - begin;
-
-        scalar_t offset = outer_offsets[m] + inner_offsets[m * T + tid];
-        at::vec::map(
-            [=](Vectorized<scalar_t> x) {
-              return x + Vectorized<scalar_t>(offset);
-            },
-            result_ptr,
-            result_ptr,
-            len);
-      }
-    });
-
-    // reinit inner offset value
-    std::fill(inner_offsets.begin(), inner_offsets.end(), scalar_t(0));
-
-    // update outer offset value
-    for (int64_t m = 0; m < M; m++) {
-      outer_offsets[m] = result_data[m * N + k_end - 1];
-    }
-  }
+    at::Tensor& activation,
+    at::Tensor& weight,
+    at::Tensor& output,
+    int64_t m,
+    int64_t n,
+    int64_t k) {
+  std::cout << "enter calc" << std::endl;
+  auto a = activation.data_ptr<float>();
+  auto b = weight.data_ptr<float>();
+  auto c = output.data_ptr<float>();
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      for (int kk = 0; kk < k; kk++)
+        *(c + i * n + j) += *(a + i * k + kk) * *(b + kk * n + j);
 }
 
 bool cumsum_fast_path(
@@ -151,24 +67,30 @@ bool cumsum_fast_path(
 
 class NewCumSumOp : public torch::autograd::Function<NewCumSumOp> {
  public:
-  static at::Tensor _forward(
-      at::Tensor& result,
-      const at::Tensor& self,
-      int64_t dim,
-      c10::optional<at::ScalarType> dtype) {
-    RECORD_FUNCTION("IPEXCumSumOp::_forward", c10::ArrayRef<c10::IValue>({}));
+  static at::Tensor& _forward(
+      at::Tensor& activation,
+      at::Tensor& weight,
+      at::Tensor& output,
+      int64_t m,
+      int64_t n,
+      int64_t k) {
+    // RECORD_FUNCTION("IPEXCumSumOp::_forward",
+    // c10::ArrayRef<c10::IValue>({}));
 
-    if (result.sizes() != self.sizes()) {
-      at::native::resize_output(result, self.sizes());
-    }
-    if (cumsum_fast_path(result, self, dim, dtype)) {
-      AT_DISPATCH_FLOATING_TYPES_AND(
-          at::ScalarType::Long, self.scalar_type(), "cumsum_lastdim_cpu", [&] {
-            cumsum_lastdim_kernel<scalar_t>(result, self, dim);
-          });
-      return result;
-    }
-    return at::cumsum_out(result, self, dim, dtype);
+    // if (result.sizes() != self.sizes()) {
+    //   at::native::resize_output(result, self.sizes());
+    // }
+    // if (cumsum_fast_path(result, self, dim, dtype)) {
+    //   AT_DISPATCH_FLOATING_TYPES_AND(
+    //       at::ScalarType::Long, self.scalar_type(), "cumsum_lastdim_cpu", [&]
+    //       {
+    std::cout << "enter fwd" << std::endl;
+    cumsum_lastdim_kernel(activation, weight, output, m, n, k);
+    return output;
+    //       });
+    //   return result;
+    // }
+    // return at::cumsum_out(result, self, dim, dtype);
   }
 
   static at::Tensor forward(
@@ -181,8 +103,8 @@ class NewCumSumOp : public torch::autograd::Function<NewCumSumOp> {
 
     at::AutoDispatchBelowADInplaceOrView g;
     ctx->saved_data["dim"] = dim;
-    auto ret = _forward(result, self, dim, dtype);
-    return ret;
+    // auto ret = _forward(result, self, dim, dtype);
+    return result;
   }
 
   static torch::autograd::tensor_list backward(
@@ -203,14 +125,17 @@ class NewCumSumOp : public torch::autograd::Function<NewCumSumOp> {
   }
 };
 
-at::Tensor cumsum_kernel_impl(
-    at::Tensor& result,
-    const at::Tensor& self,
-    int64_t dim,
-    c10::optional<at::ScalarType> dtype) {
-  if (at::GradMode::is_enabled() && self.requires_grad())
-    return NewCumSumOp::apply(result, self, dim, dtype);
-  return NewCumSumOp::_forward(result, self, dim, dtype);
+at::Tensor& cumsum_kernel_impl(
+    at::Tensor& activation,
+    at::Tensor& weight,
+    at::Tensor& output,
+    int64_t m,
+    int64_t n,
+    int64_t k) {
+  // if (at::GradMode::is_enabled() && self.requires_grad())
+  //   return NewCumSumOp::apply(result, self, dim, dtype);
+  std::cout << "enter cumsum kernel impl" << std::endl;
+  return NewCumSumOp::_forward(activation, weight, output, m, n, k);
 }
 
 } // anonymous namespace
