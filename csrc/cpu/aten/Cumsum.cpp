de244cb06fe7b9272b7896b359ae910f8c53f7b7
Wang,Zhe <zhe1.wang@intel.com>
2023-09-26 14:16:00 +0800

    revert cumsum ker

--- csrc/cpu/aten/Cumsum.cpp
+++ csrc/cpu/aten/Cumsum.cpp
@@ -1,74 +1,65 @@
+#include "Cumsum.h"
 #include <ATen/NamedTensorUtils.h>
 #include <ATen/WrapDimUtils.h>
-#include <ATen/core/TensorBody.h>
 #include <ATen/cpu/vec/functional.h>
 #include <ATen/cpu/vec/vec.h>
 #include <ATen/native/ReduceOpsUtils.h>
 #include <ATen/native/TensorIterator.h>
 #include <immintrin.h>
-#include <cstdint>
-#include "Cumsum.h"
 
 namespace torch_ipex {
 namespace cpu {
 
 DEFINE_DISPATCH(cumsum_kernel_stub);
 
-at::Tensor& cumsum(
-    at::Tensor& activation,
-    at::Tensor& weight,
-    at::Tensor& output,
-    int64_t m,
-    int64_t n,
-    int64_t k) {
-  // auto casted_self = at::native::integer_upcast(self, dtype);
-  // at::Tensor result = at::empty_like(casted_self,
-  // at::MemoryFormat::Contiguous);
+at::Tensor cumsum(
+    const at::Tensor& self,
+    int64_t dim,
+    c10::optional<at::ScalarType> dtype) {
+  auto casted_self = at::native::integer_upcast(self, dtype);
+  at::Tensor result = at::empty_like(casted_self, at::MemoryFormat::Contiguous);
 
-  // // pointer to cumsum_kernel_impl(result, casted_self, dim, dtype);
-  // return cumsum_kernel_stub(kCPU, result, casted_self, dim, dtype);
-  std::cout << "enter cumsum func" << std::endl;
-  return cumsum_kernel_stub(kCPU, activation, weight, output, m, n, k);
+  // pointer to cumsum_kernel_impl(result, casted_self, dim, dtype);
+  return cumsum_kernel_stub(kCPU, result, casted_self, dim, dtype);
 }
 
-// at::Tensor& cumsum_(
-//     at::Tensor& self,
-//     int64_t dim,
-//     c10::optional<at::ScalarType> dtype) {
-//   // pointer to cumsum_kernel_impl(self, self, dim, dtype);
-//   cumsum_kernel_stub(kCPU, self, self, dim, dtype);
+at::Tensor& cumsum_(
+    at::Tensor& self,
+    int64_t dim,
+    c10::optional<at::ScalarType> dtype) {
+  // pointer to cumsum_kernel_impl(self, self, dim, dtype);
+  cumsum_kernel_stub(kCPU, self, self, dim, dtype);
 
-//   return self;
-// }
+  return self;
+}
 
-// at::Tensor& cumsum_out(
-//     const at::Tensor& self,
-//     int64_t dim,
-//     c10::optional<at::ScalarType> dtype,
-//     at::Tensor& result) {
-//   // cumsum_kernel_impl(result, self.toType(result.scalar_type()), dim,
-//   dtype); cumsum_kernel_stub(
-//       kCPU, result, self.toType(result.scalar_type()), dim, dtype);
+at::Tensor& cumsum_out(
+    const at::Tensor& self,
+    int64_t dim,
+    c10::optional<at::ScalarType> dtype,
+    at::Tensor& result) {
+  // cumsum_kernel_impl(result, self.toType(result.scalar_type()), dim, dtype);
+  cumsum_kernel_stub(
+      kCPU, result, self.toType(result.scalar_type()), dim, dtype);
 
-//   return result;
-// }
+  return result;
+}
 
 } // namespace cpu
 
 namespace {
 
 TORCH_LIBRARY_FRAGMENT(torch_ipex, m) {
-  m.def(
-      "cumsum(Tensor activation, Tensor weight, Tensor(a!) output, int m, int n, int k) -> Tensor(a!)");
+  m.def("cumsum(Tensor self, int dim, *, ScalarType? dtype=None) -> Tensor");
   m.impl("cumsum", c10::DispatchKey::CPU, torch_ipex::cpu::cumsum);
-  // m.def(
-  //     "cumsum_(Tensor(a!) self, int dim, *, ScalarType? dtype=None) -> "
-  //     "Tensor(a!)");
-  // m.impl("cumsum_", c10::DispatchKey::CPU, torch_ipex::cpu::cumsum_);
-  // m.def(
-  //     "cumsum.out(Tensor self, int dim, *, ScalarType? dtype=None, "
-  //     "Tensor(a!) out) -> Tensor(a!)");
-  // m.impl("cumsum.out", c10::DispatchKey::CPU, torch_ipex::cpu::cumsum_out);
+  m.def(
+      "cumsum_(Tensor(a!) self, int dim, *, ScalarType? dtype=None) -> "
+      "Tensor(a!)");
+  m.impl("cumsum_", c10::DispatchKey::CPU, torch_ipex::cpu::cumsum_);
+  m.def(
+      "cumsum.out(Tensor self, int dim, *, ScalarType? dtype=None, "
+      "Tensor(a!) out) -> Tensor(a!)");
+  m.impl("cumsum.out", c10::DispatchKey::CPU, torch_ipex::cpu::cumsum_out);
 }
 
 } // namespace
--- csrc/cpu/aten/Cumsum.h
+++ csrc/cpu/aten/Cumsum.h
@@ -1,30 +1,25 @@
 #include <ATen/Tensor.h>
-#include <ATen/core/TensorBody.h>
 #include <dyndisp/DispatchStub.h>
 #include <torch/all.h>
-#include <cstdint>
 
 namespace torch_ipex {
 namespace cpu {
 
 namespace {
 
-// naive gemm, not jblas version
-at::Tensor& cumsum_kernel_impl(
-    at::Tensor& activation,
-    at::Tensor& weight,
-    at::Tensor& output,
-    int64_t m,
-    int64_t n,
-    int64_t k);
-} // namespace
+at::Tensor cumsum_kernel_impl(
+    at::Tensor& result,
+    const at::Tensor& self,
+    int64_t dim,
+    c10::optional<at::ScalarType> dtype);
 
-using cumsum_kernel_fn = at::Tensor& (*)(at::Tensor&,
-                                         at::Tensor&,
-                                         at::Tensor&,
-                                         int64_t,
-                                         int64_t,
-                                         int64_t);
+}
+
+using cumsum_kernel_fn = at::Tensor (*)(
+    at::Tensor&,
+    const at::Tensor&,
+    int64_t,
+    c10::optional<at::ScalarType>);
 DECLARE_DISPATCH(cumsum_kernel_fn, cumsum_kernel_stub);
 
 } // namespace cpu
